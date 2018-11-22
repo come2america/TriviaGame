43d1cb4478b51678a8ed9e9bc88bd1483901734e
Yusuf <ysserunjogi@gmail.com>
2018-11-22 10:56:15 -0800

    set up done but button checker show one result of all

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-    var question = [
+    var ques = [
         {
-          question1: "Which music video premiered in 27 countries in 1991",
+          question: "Which music video premiered in 27 countries in 1991",
           answers: ["Scream",
           ,"Hollywood Tonight","We R The World", "Black or White"],
           correctAnswer: "Black or White"
         },
         {
-          question2: "Which song's short film in 1988 earned the People's Choice Award",
+          question: "Which song's short film in 1988 earned the People's Choice Award",
          answers: ["Thriller","Beat It","Remember The Time","Smooth Crimminal"],
          correctAnswer: "Smooth Crimminal"
         },
         {
-          question3: "Which song has parts recorded with him lying in bed, under covers ",
+          question: "Which song has parts recorded with him lying in bed, under covers ",
           answers:  ["Speechless","Leave Me Alone","Cry","I Just Can't Stop Loving You"],
           correctAnswer:"I Just Can't Stop Loving You"
         },
@@ -20,25 +20,42 @@ $(document).ready(function () {
       ];
       var questionanswer;
  function displayques () {
-         questionanswer=question[Math.floor((Math.random() * question.length))];
+         questionanswer=ques[Math.floor((Math.random() * ques.length))];
           console.log(questionanswer)  
                // $("#question-holder").append("<div>" + question + "</div>");
             
    return questionanswer     
 }
 function appendque( ){
-    $("#question-holder").html("<div>" + questionanswer  + "</div>");
-}
+    $("#question-holder").html("<h1>" + questionanswer.question+ "</h1>");
+    for (var i = 0; i < questionanswer.answers.length ; i++) {
+    $("#question-holder").append( "<button class='btn'>" + questionanswer.answers[i]+ "</button>");
+ }
+}''
+
+function checker(){
+  if ($(".btn")== questionanswer.correctAnswer) {
+   console.log("good job")
+ }
+ else {
+  console.log("bad job")
+
+ }
+    
+  }
 
 
 
-    $("#start").click(function(){ 
-        displayques(
+    $("#start").click(function(){  
+        displayques(   );
+      appendque();
+    }) 
+   
+    $("#question-holder").on('click',".btn", function(){
 
-        );
-      appendque();  
-    })
-
+     checker();
+  
+  });
 
 
    
